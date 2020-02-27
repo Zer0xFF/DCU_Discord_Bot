9c76a8661fb9ca7b0c3b096051fde0e62ed69c3c
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-02-27 14:36:41 +0000

    refactor, bring this closer to upstream

--- cogs/ai_dungeon.py
+++ cogs/ai_dungeon.py
@@ -1,31 +1,59 @@
 import sys
-sys.path.append('../ai-dungeon-cli/')
+sys.path.append('../ai_dungeon_cli/')
 
-import ai_dungeon_cli
-from ai_dungeon_cli.ai_dungeon_cli import AI_D as AI
+from ai_dungeon_cli import ai_dungeon_cli
 import aiohttp
 import discord
 from discord.ext import commands
+import collections
 
 
 class AI_Dungeon(commands.Cog):
-    """Dublin Bus Times"""
-
+    """I_Dungeon"""
     def __init__(self, bot):
         self.bot = bot
-        self.session = AI()
-        self.session.start_session()
+        self.pending_msg = collections.deque()
+        self.pending_input = collections.deque()
+        self.session = ai_dungeon_cli.AiDungeon()
+        ai_dungeon_cli.set_input(lambda *args : self.pending_input.popleft())
+        ai_dungeon_cli.set_print(lambda txt : self.discord_queue_msg(txt))
+
+        # Login if necessary
+        if not self.session.get_auth_token():
+            self.session.login()
+
+        # Update the session authentication token
+        self.session.update_session_auth()
+
 
     def cog_unload(self):
         pass
 
+    async def discord_print_msg(self):
+        if(len(self.pending_msg) > 0):
+            await self.pending_msg.pop()
+
+    def discord_queue_msg(self, txt):
+        if(self.send):
+            self.pending_msg.append(self.send("```{}```".format(txt)))
+        else:
+            print(txt)
+
     @commands.has_any_role("OVERLORDS", "Mahmood")
     @commands.command()
-    async def start_game(self, ctx):
+    async def start_game(self, ctx, setting_id, character_id, character_name):
         """Ai Dungeon"""
-        self.session.set_context(ctx)
-        self.session.start_game()
-        await self.session.msg()
+        self.send = ctx.send
+
+        self.pending_input.append(setting_id)
+        self.pending_input.append(character_id)
+        self.pending_input.append(character_name)
+
+        # Loads the current session configuration
+        self.session.choose_config()
+        # Initializes the story
+        self.session.init_story()
+        await self.discord_print_msg()
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -40,12 +68,11 @@ class AI_Dungeon(commands.Cog):
             return
 
         if(message.channel.name == "ai_dungeon2"):
-            res = self.session.process_next_action(message.content)
-            self.session.set_context(message.channel)
-            await self.session.msg()
-            action_type, action_res = res
-            if action_type == 'regular':
-                self.session.prompt_i += 2
+            self.send = message.channel.send
+            self.pending_input.append(message.content)
+
+            self.session.process_next_action()
+            await self.discord_print_msg()
 
 def setup(bot):
     bot.add_cog(AI_Dungeon(bot))
