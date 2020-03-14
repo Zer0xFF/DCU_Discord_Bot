a989bc88ab3dcb8abdf58c59df8cca7019f170d9
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-03-14 23:27:00 +0000

    update covid implementation, return more states and allow individual country selection

--- cogs/covid.py
+++ cogs/covid.py
@@ -20,19 +20,32 @@ class Covid(commands.Cog):
             return await response.text()
 
     @commands.command(aliases=["covid-19", "coronavirus", "covid", "corona"])
-    async def get_covid(self, ctx):
+    async def get_covid(self, ctx, country=None):
         """COVID-19 RealTime Info"""
         res = await self.get(
                 "https://www.worldometers.info/coronavirus/")
+
         soup = BeautifulSoup(res,'html.parser')
-        rows = soup.find("div", {"class":"maincounter-number"}).find("span").text
-        embed = discord.Embed(
-                colour=ctx.author.colour,
-                title=f"Number of infected cases: ",
-                description=rows
-            )
-        await ctx.send(embed=embed)
-        await ctx.send("https://www.youtube.com/watch?v=cphNpqKpKc4")
+        headers = [h.get_text().strip() for h in soup.find("thead").find_all('th')]
+        headers[0] = 'Country'
+
+        if(country):
+            info = next(iter([c for c in soup.find("tbody").find_all('tr') if c.td.get_text().strip().casefold() == country.casefold()]), None)
+        else:
+            info = soup.find_all("tbody")[1]
+            info.td.string.replace_with("Global")
+
+        if(info):
+            embed = discord.Embed(title="Covid-19 Stats", color=ctx.author.colour)
+            data = info.find_all('td')
+            for i in range(len(headers)):
+                val = data[i].get_text().strip() if data[i].get_text().strip() else "0"
+                embed.add_field(name=headers[i], value=val)
+
+            await ctx.send(embed=embed)
+            await ctx.send("https://www.youtube.com/watch?v=cphNpqKpKc4")
+        else:
+            await ctx.send("Country not found.")
 
 def setup(bot):
     bot.add_cog(Covid(bot))
