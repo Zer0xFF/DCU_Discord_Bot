fa498608e02a44f26aa4eecc5c1c07989bd36828
Brendan <bman3210@gmail.com>
2020-11-03 01:41:52 +0000

    added variables to self initialization

--- cogs/whisper.py
+++ cogs/whisper.py
@@ -10,20 +10,20 @@ class Whisper(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.random_animals = ['Ferret', 'Donkey', 'Robin', 'Platypus', 'Moose', 'Panda', 'Aardvark', 'Dog', 'Ant', 'Cheetah', 'Chicken', 'Duck', 'Eagle', 'Gecko', 'Lion', 'Pig', 'Wolf']
+        self.random_adjectives = ['Alert', 'Alive', 'Amused', 'Clever', 'Crazy', 'Dizzy', 'Jolly', 'Brave', 'Tired', 'Busy', 'Bored']
 
     @commands.dm_only()
     @commands.command(aliases=["whisper"])
     async def psst(self, ctx, *,message: str):
         """Psst - PM the bot with the !whisper prefix,
         along with your message."""
-        
-        random_animals = ['Ferret', 'Donkey', 'Robin', 'Platypus', 'Moose', 'Panda', 'Aardvark', 'Dog', 'Ant', 'Cheetah', 'Chicken', 'Duck', 'Eagle', 'Gecko', 'Lion', 'Pig', 'Wolf'] 
-        random_adjectives = ['Alert', 'Alive', 'Amused', 'Clever', 'Crazy', 'Dizzy', 'Jolly', 'Brave', 'Tired', 'Busy', 'Bored']
+
 
         seed = os.environ.get('WHISPERHASH')
         channel = self.bot.get_channel(689655916223660095)
         namehash = abs(hash(ctx.message.author.mention)*hash(seed)) % (10 ** 8)
-        await channel.send(random_adjectives[namehash % len(random_adjectives)] + " " + random_animals[namehash -24 % len(random_animals)] + ":  " + message)
+        await channel.send(self.random_adjectives[namehash % len(self.random_adjectives)] + " " + self.random_animals[namehash -24 % len(self.random_animals)] + ":  " + message)
 
 
 def setup(bot):
